df898809ce3d92876136b1d42083850419266af2
Hideyo Gotoda <16.hide.13.love@gmail.com>
2020-12-23 22:04:59 +0900

    .gitignore is now working

--- README.md
+++
@@ -1,9 +0,0 @@
-# practice_code
-コードの練習です
-- HTML&CSS
-- Java Script
-- jQuery
-- Ruby
-- Python
-
-ついでにgitの練習もかねて
--- python/20201211.py
+++
@@ -1,9 +0,0 @@
-# 2020-12-11
-animals = ['dog', 'cat', 'bird', 'mouth']
-for animal in animals:
-    print(animal)
-
-def times(a,b,c):
-    return a*b*c
-print(times(1,2,3))
-print(times(2,3,4))
